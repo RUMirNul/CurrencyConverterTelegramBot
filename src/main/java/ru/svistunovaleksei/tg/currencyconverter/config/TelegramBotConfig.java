f184928e317b70348a9d3bbda0673cce85745de8
lexacv01@gmail.com <Agent3228>
2022-08-18 18:38:47 +0300

    Replaced getters and setters on lombok annotations

--- src/main/java/ru/svistunovaleksei/tg/currencyconverter/config/TelegramBotConfig.java
+++ src/main/java/ru/svistunovaleksei/tg/currencyconverter/config/TelegramBotConfig.java
@@ -1,39 +1,17 @@
 package ru.svistunovaleksei.tg.currencyconverter.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "app.telegram-bot")
+@Getter
+@Setter
 public class TelegramBotConfig {
 
     private String name;
-
     private String token;
-
     private String webHookPath;
-
-    public String getName() {
-        return name;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public String getWebHookPath() {
-        return webHookPath;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public void setWebHookPath(String webHookPath) {
-        this.webHookPath = webHookPath;
-    }
 }
